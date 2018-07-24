81e9d89445d189d1139b018fad434c4e097307c3
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-07-24 13:11:27 -0700

    updated learn to also support slightly modified questions during learning

--- tutorbot/management/commands/learn.py
+++ tutorbot/management/commands/learn.py
@@ -4,6 +4,12 @@ from tutorbot.models import Question as Question, Answer as Answer
 from chatterbot.ext.django_chatterbot.models import Statement
 import json
 
+similar_q_prefix_pairs = [
+    ['what do you understand by', 'what is'],
+    ['explain how', 'how'],
+    ['explain what', 'what'],
+]
+
 # ref: https://docs.djangoproject.com/en/1.11/howto/custom-management-commands/
 class Command(BaseCommand):
     help = 'Learns from gathered new knowledge'
@@ -23,14 +29,25 @@ class Command(BaseCommand):
         else:
             self.stdout.write('Found new questions. Learning!')
             for q in qs:
-                qs = Question.objects.filter(text=q.text)
-                chatterbot.train([q.text, q.answer.summary])
+                q_texts = self.create_similar_questions(q.text)
+                # print ("----")
+                for q_text in q_texts:
+                    # print (q_text)
+                    chatterbot.train([q_text, q.answer.summary])
+                # print ("----")
                 self.update_answer_statement_data(q.answer, q.answer.summary)
                 q.learned=True
                 q.save()
                 count += 1
 
         self.stdout.write(self.style.SUCCESS('Knowledge learning complete. Found %d new knowledge items!' % count))
+
+    def create_similar_questions(self, text):
+        for q_prefix in similar_q_prefix_pairs:
+            if text.lower().startswith(q_prefix[0]):
+                list = []
+                return [text, text.lower().replace(q_prefix[0], q_prefix[1], 1)]
+        return [text]
 
     def update_answer_statement_data(self, answer, answer_summary):
         statement_qs = Statement.objects.filter(text=answer_summary)
